f693a64d6e18f440c18012f8ff3485a6f44c1363
Nikita Melnikov <nikita.melnikov@and.digital>
2019-11-08 16:43:58 +0000

    Pulled message data from Slack and added Redis integration

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,18 @@
 const express = require('express')
 const routes = require('./routes/api')
 const dotenv = require('dotenv').config();
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+    console.error(err);
+});
 
 const app = express();
 app.use(routes);
 
-
-
 app.listen(process.env.port || 4000, function(){
-    console.log("hello world")
+    console.log(`Server started`);
 });
-
-app.get('/',function(req,res){
-    console.log("endpoint");
-    res.send("Response");
-})
-
 
--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const { WebClient } = require('@slack/web-api');
 
-router.get('/hello',function(req,res){
-    res.send("a")
+const redis = require('redis');
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+    console.error(err);
 });
 
-router.get('/hello2',function(req,res){
-    res.send("b")
+const token = process.env.SLACK_TOKEN;
+const web = new WebClient(token);
+
+router.get('/', function(req,res){
+    (async () => {
+        if (!process.env.SLACK_TOKEN) {
+            console.error(`No Slack Token provided`);
+            return;
+        }
+
+        const result = await web.channels.history({
+            token: process.env.SLACK_TOKEN,
+            channel: `C6JGGFGEM`,
+            count: 10,
+        });
+        
+        // Filter messages and get rid of those without any text
+        const messages = result.messages
+            .filter(message => message.text.length > 0);
+
+        let count = 0;
+        // Add each message to Redis db
+        messages.forEach(message => {
+            console.log(JSON.stringify(message));
+            client.set(message.client_message_id || count++, JSON.stringify(message), redis.print);
+        });
+        res.send(messages);
+    })();
+});
+
+router.get('/test',function(req,res){
+    const result = client.get(`f87b102b-6b7a-432c-ba2a-8892ab101852`, (err, data) => {
+        if (err) console.error(err);
+        else {
+            res.send(data);
+        }
+    });
 });
 
 router.get('/hello3',function(req,res){
