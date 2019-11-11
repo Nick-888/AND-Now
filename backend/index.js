0fe35a1f2a25b4b629a8c9398500c3f9319444f1
Nikita Melnikov <nikita.melnikov@and.digital>
2019-11-11 10:19:22 +0000

    Tidy up of index.js

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,6 @@
 const express = require('express')
 const routes = require('./routes/api')
 const dotenv = require('dotenv').config();
-const redis = require('redis');
-
-const client = redis.createClient();
-
-client.on('error', (err) => {
-    console.error(err);
-});
 
 const app = express();
 app.use(routes);
